91a14c3073203815d713d311595e5dea6274af79
Nishant Srivastava <nisrulz@gmail.com>
2016-09-21 13:45:58 +0530

    [*] minor fixes

--- recyclerviewhelper/src/main/java/github/nisrulz/recyclerviewhelper/RVHAdapter.java
+++ recyclerviewhelper/src/main/java/github/nisrulz/recyclerviewhelper/RVHAdapter.java
@@ -7,9 +7,9 @@ public interface RVHAdapter {
 
   /**
    * Called when an item has been dragged far enough to trigger a move. This is called every time
-   * an item is shifted, and <strong>not</strong> at the end of a "drop" event.<br/>
-   * <br/>
-   * Implementations should call RecyclerView.Adapter#notifyItemMoved(int, int) after
+   * an item is shifted, and not at the end of a "drop" event.
+   *
+   * Implementations should call RecyclerView.Adapter notifyItemMoved(int, int) after
    * adjusting the underlying data to reflect this move.
    *
    * @param fromPosition
@@ -21,9 +21,9 @@ public interface RVHAdapter {
   boolean onItemMove(int fromPosition, int toPosition);
 
   /**
-   * Called when an item has been dismissed by a swipe.<br/>
-   * <br/>
-   * Implementations should call RecyclerView.Adapter#notifyItemRemoved(int) after
+   * Called when an item has been dismissed by a swipe.
+   *
+   * Implementations should call RecyclerView.Adapter notifyItemRemoved(int) after
    * adjusting the underlying data to reflect this removal.
    *
    * @param position
