87f88d227d96940253dcbbdfe6b64ad5025c73fd
Nishant Srivastava <nisrulz@gmail.com>
2017-11-08 16:52:21 -0800

    reformatted code

--- app/src/main/java/github/nisrulz/projectrecyclerviewhelper/MainActivity.java
+++ app/src/main/java/github/nisrulz/projectrecyclerviewhelper/MainActivity.java
@@ -31,62 +31,62 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-  private ArrayList<String> data;
+    private ArrayList<String> data;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    RecyclerView myrecyclerview = findViewById(R.id.rv_fruits);
+        RecyclerView myrecyclerview = findViewById(R.id.rv_fruits);
 
-    data = new ArrayList<>();
-    data.add("Apple");
-    data.add("Banana");
-    data.add("Peach");
-    data.add("Pineapple");
-    data.add("Orange");
-    data.add("Strawberry");
-    data.add("Grapes");
-    data.add("Apricot");
-    data.add("Avocado");
-    data.add("Raisin");
-    data.add("Guava");
-    data.add("Papaya");
-    data.add("Pear");
-    data.add("Blueberry");
-    data.add("Lychee");
-    data.add("Date");
-    data.add("Fig");
+        data = new ArrayList<>();
+        data.add("Apple");
+        data.add("Banana");
+        data.add("Peach");
+        data.add("Pineapple");
+        data.add("Orange");
+        data.add("Strawberry");
+        data.add("Grapes");
+        data.add("Apricot");
+        data.add("Avocado");
+        data.add("Raisin");
+        data.add("Guava");
+        data.add("Papaya");
+        data.add("Pear");
+        data.add("Blueberry");
+        data.add("Lychee");
+        data.add("Date");
+        data.add("Fig");
 
-    MyAdapter adapter = new MyAdapter(data);
-    myrecyclerview.hasFixedSize();
-    myrecyclerview.setLayoutManager(new LinearLayoutManager(this));
-    myrecyclerview.setAdapter(adapter);
+        MyAdapter adapter = new MyAdapter(data);
+        myrecyclerview.hasFixedSize();
+        myrecyclerview.setLayoutManager(new LinearLayoutManager(this));
+        myrecyclerview.setAdapter(adapter);
 
-    // Setup onItemTouchHandler
-    ItemTouchHelper.Callback callback = new RVHItemTouchHelperCallback(adapter, true, true, true);
-    ItemTouchHelper helper = new ItemTouchHelper(callback);
-    helper.attachToRecyclerView(myrecyclerview);
+        // Setup onItemTouchHandler
+        ItemTouchHelper.Callback callback = new RVHItemTouchHelperCallback(adapter, true, true, true);
+        ItemTouchHelper helper = new ItemTouchHelper(callback);
+        helper.attachToRecyclerView(myrecyclerview);
 
-    // Set the divider
-    myrecyclerview.addItemDecoration(
-        new RVHItemDividerDecoration(this, LinearLayoutManager.VERTICAL));
+        // Set the divider
+        myrecyclerview.addItemDecoration(
+                new RVHItemDividerDecoration(this, LinearLayoutManager.VERTICAL));
 
-    // Set On Click
-    myrecyclerview.addOnItemTouchListener(
-        new RVHItemClickListener(this, new RVHItemClickListener.OnItemClickListener() {
-          @Override
-          public void onItemClick(View view, int position) {
-            String value = "Clicked Item " + data.get(position) + " at " + position;
+        // Set On Click
+        myrecyclerview.addOnItemTouchListener(
+                new RVHItemClickListener(this, new RVHItemClickListener.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+                        String value = "Clicked Item " + data.get(position) + " at " + position;
 
-            Log.d("TAG", value);
-            Toast.makeText(MainActivity.this, value, Toast.LENGTH_SHORT).show();
-          }
-        }));
+                        Log.d("TAG", value);
+                        Toast.makeText(MainActivity.this, value, Toast.LENGTH_SHORT).show();
+                    }
+                }));
 
-    // Show Toast
-    Toast.makeText(MainActivity.this, "Swipe items left/right\nLong press and drag and drop",
-        Toast.LENGTH_LONG).show();
-  }
+        // Show Toast
+        Toast.makeText(MainActivity.this, "Swipe items left/right\nLong press and drag and drop",
+                Toast.LENGTH_LONG).show();
+    }
 }
--- app/src/main/java/github/nisrulz/projectrecyclerviewhelper/MyAdapter.java
+++ app/src/main/java/github/nisrulz/projectrecyclerviewhelper/MyAdapter.java
@@ -27,70 +27,71 @@ import java.util.Collections;
 import java.util.List;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ItemViewHolder>
-    implements RVHAdapter {
+        implements RVHAdapter {
 
-  private final List<String> dataList;
+    public class ItemViewHolder extends RecyclerView.ViewHolder implements RVHViewHolder {
 
-  public MyAdapter(List<String> dataList) {
-    this.dataList = dataList;
-  }
+        final TextView txt;
 
-  @Override
-  public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        public ItemViewHolder(View itemView) {
+            super(itemView);
+            txt = itemView.findViewById(R.id.txt);
+        }
 
-    View view =
-        LayoutInflater.from(parent.getContext()).inflate(R.layout.rv_list_item, parent, false);
+        @Override
+        public void onItemSelected(int actionstate) {
+            System.out.println("Item is selected");
+        }
 
-    return new ItemViewHolder(view);
-  }
+        @Override
+        public void onItemClear() {
+            System.out.println("Item is unselected");
+        }
+    }
 
-  @Override
-  public void onBindViewHolder(ItemViewHolder holder, int position) {
-    holder.txt.setText(dataList.get(position));
-  }
+    private final List<String> dataList;
 
-  @Override
-  public int getItemCount() {
-    return dataList.size();
-  }
-
-  @Override
-  public boolean onItemMove(int fromPosition, int toPosition) {
-    swap(fromPosition, toPosition);
-    return false;
-  }
-
-  @Override
-  public void onItemDismiss(int position, int direction) {
-    remove(position);
-  }
-
-  private void remove(int position) {
-    dataList.remove(position);
-    notifyItemRemoved(position);
-  }
-
-  private void swap(int firstPosition, int secondPosition) {
-    Collections.swap(dataList, firstPosition, secondPosition);
-    notifyItemMoved(firstPosition, secondPosition);
-  }
-
-  public class ItemViewHolder extends RecyclerView.ViewHolder implements RVHViewHolder {
-    final TextView txt;
-
-    public ItemViewHolder(View itemView) {
-      super(itemView);
-      txt = itemView.findViewById(R.id.txt);
+    public MyAdapter(List<String> dataList) {
+        this.dataList = dataList;
     }
 
     @Override
-    public void onItemSelected(int actionstate) {
-      System.out.println("Item is selected");
+    public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view =
+                LayoutInflater.from(parent.getContext()).inflate(R.layout.rv_list_item, parent, false);
+
+        return new ItemViewHolder(view);
     }
 
     @Override
-    public void onItemClear() {
-      System.out.println("Item is unselected");
+    public void onBindViewHolder(ItemViewHolder holder, int position) {
+        holder.txt.setText(dataList.get(position));
     }
-  }
+
+    @Override
+    public int getItemCount() {
+        return dataList.size();
+    }
+
+    @Override
+    public boolean onItemMove(int fromPosition, int toPosition) {
+        swap(fromPosition, toPosition);
+        return false;
+    }
+
+    @Override
+    public void onItemDismiss(int position, int direction) {
+        remove(position);
+    }
+
+    private void remove(int position) {
+        dataList.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    private void swap(int firstPosition, int secondPosition) {
+        Collections.swap(dataList, firstPosition, secondPosition);
+        notifyItemMoved(firstPosition, secondPosition);
+    }
 }
